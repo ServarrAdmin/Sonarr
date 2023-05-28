jamesmacwhite/Lidarr

New: Postgres Database Support
Co-Authored-By: Qstick <376117+Qstick@users.noreply.github.com>

src/NzbDrone.Core/Housekeeping/Housekeepers/CleanupOrphanedHistoryItems.cs
@@ -22,12 +22,12 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
         {
             using (var mapper = _database.OpenConnection())
             {
-                mapper.Execute(@"DELETE FROM History
-                                     WHERE Id IN (
-                                     SELECT History.Id FROM History
-                                     LEFT OUTER JOIN Artists
-                                     ON History.ArtistId = Artists.Id
-                                     WHERE Artists.Id IS NULL)");
+                mapper.Execute(@"DELETE FROM ""History""
+                                     WHERE ""Id"" IN (
+                                     SELECT ""History"".""Id"" FROM ""History""
+                                     LEFT OUTER JOIN ""Artists""
+                                     ON ""History"".""ArtistId"" = ""Artists"".""Id""
+                                     WHERE ""Artists"".""Id"" IS NULL)");
             }
         }
 
@@ -35,12 +35,12 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
         {
             using (var mapper = _database.OpenConnection())
             {
-                mapper.Execute(@"DELETE FROM History
-                                     WHERE Id IN (
-                                     SELECT History.Id FROM History
-                                     LEFT OUTER JOIN Albums
-                                     ON History.AlbumId = Albums.Id
-                                     WHERE Albums.Id IS NULL)");
+                mapper.Execute(@"DELETE FROM ""History""
+                                     WHERE ""Id"" IN (
+                                     SELECT ""History"".""Id"" FROM ""History""
+                                     LEFT OUTER JOIN ""Albums""
+                                     ON ""History"".""AlbumId"" = ""Albums"".""Id""
+                                     WHERE ""Albums"".""Id"" IS NULL)");
             }
         }
     }

ServarrAdmin/Sonarr

src/NzbDrone.Core/Housekeeping/Housekeepers/CleanupOrphanedHistoryItems.cs
@@ -22,12 +22,12 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
         {
             using (var mapper = _database.OpenConnection())
             {
-                mapper.Execute(@"DELETE FROM History
-                                     WHERE Id IN (
-                                     SELECT History.Id FROM History
-                                     LEFT OUTER JOIN Series
-                                     ON History.SeriesId = Series.Id
-                                     WHERE Series.Id IS NULL)");
+                mapper.Execute(@"DELETE FROM ""History""
+                                     WHERE ""Id"" IN (
+                                     SELECT ""History"".""Id"" FROM ""History""
+                                     LEFT OUTER JOIN ""Series""
+                                     ON ""History"".""SeriesId"" = ""Series"".""Id""
+                                     WHERE ""Series"".""Id"" IS NULL)");
             }
         }
 
@@ -35,12 +35,12 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
         {
             using (var mapper = _database.OpenConnection())
             {
-                mapper.Execute(@"DELETE FROM History
-                                     WHERE Id IN (
-                                     SELECT History.Id FROM History
-                                     LEFT OUTER JOIN Episodes
-                                     ON History.EpisodeId = Episodes.Id
-                                     WHERE Episodes.Id IS NULL)");
+                mapper.Execute(@"DELETE FROM ""History""
+                                     WHERE ""Id"" IN (
+                                     SELECT ""History"".""Id"" FROM ""History""
+                                     LEFT OUTER JOIN ""Episodes""
+                                     ON ""History"".""EpisodeId"" = ""Episodes"".""Id""
+                                     WHERE ""Episodes"".""Id"" IS NULL)");
             }
         }
     }